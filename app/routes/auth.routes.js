985ab4b0596fad40043b83aafec10b4363990e44
Kevin Octaviano <76242149+kevinoctaviano@users.noreply.github.com>
2022-06-09 11:20:51 +0700

    cors update auth.routes

--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -1,23 +1,27 @@
-const { verifySignUp } = require("../middlewares");
-const controller = require("../controllers/auth.controller");
+const { verifySignUp } = require('../middlewares');
+const controller = require('../controllers/auth.controller');
 
-module.exports = function(app) {
-  app.use(function(req, res, next) {
+module.exports = function (app) {
+  app.use(function (req, res, next) {
     res.header(
-      "Access-Control-Allow-Headers",
-      "x-access-token, Origin, Content-Type, Accept"
+      'Access-Control-Allow-Headers',
+      'x-access-token, Origin, Content-Type, Accept'
+    );
+    res.setHeader(
+      'Access-Control-Allow-Origin',
+      'https://challenge08-binar.netlify.app/'
     );
     next();
   });
 
   app.post(
-    "/api/auth/signup",
+    '/api/auth/signup',
     [
       verifySignUp.checkDuplicateUsernameOrEmail,
-      verifySignUp.checkRolesExisted
+      verifySignUp.checkRolesExisted,
     ],
     controller.signup
   );
 
-  app.post("/api/auth/signin", controller.signin);
+  app.post('/api/auth/signin', controller.signin);
 };
